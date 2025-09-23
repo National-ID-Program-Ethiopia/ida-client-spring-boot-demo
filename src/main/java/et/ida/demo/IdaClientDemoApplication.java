9842f76f3008f907dd382f72cb8a04abf7c87c8b
Amanuel <140692001+jony1748@users.noreply.github.com>
2025-09-23 21:23:38 +0300

    ida demo

---
+++ src/main/java/et/ida/demo/IdaClientDemoApplication.java
@@ -0,0 +1,13 @@
+package et.ida.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdaClientDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdaClientDemoApplication.class, args);
+	}
+
+}
