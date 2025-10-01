d40cb92b798bb24262b98a786abe690d8b6fa230
Amanuel <140692001+jony1748@users.noreply.github.com>
2025-10-01 09:45:39 +0300

    ida demo spring

--- src/test/java/et/ida/demo/DemoApplicationTests.java
+++ src/test/java/et/ida/demo/DemoApplicationTests.java
@@ -1,13 +1,13 @@
-package et.ida.demo;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DemoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package et.ida.demo;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DemoApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
